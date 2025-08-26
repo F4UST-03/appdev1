088f8d956f394d49199c23e493d918fc0cb8a233
F4UST-O3 <pantallanojojo1994@gmail.com>
2025-08-27 03:22:43 +0800

    Activity 10: Add React About Me project

---
+++ 10_getting-started-react/src/App.jsx
@@ -0,0 +1,47 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>About Me</h1>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxdVcGIJDWvg2jyNh2Q8qVb2c9SwxB0ILpv5DycSJxlOsggiC4EriE52AmjZvLKYSdzHw&usqp=CAU" alt="arab cat" />
+      <p><strong>Name: </strong>Jorilyn O. Pantallano</p>
+      <p><strong>Course & Year: </strong>Bachelor of Science in Information Systems 3</p>
+      <p><strong>Fun fact: </strong>I like performing & psychology.</p>
+      <br />
+      <strong>Why do I want to learn React?</strong>
+      <p>I want to learn React because I want to expand my knowledge in web development and it is also popular among companies. </p>
+      <p>I also want to learn React because it is a good skill to have in the job market.</p>
+      <p>In my developing experience, I've used Laravel which can sometimes or most have issues when it comes to my device that is fully secured.</p>
+      <p>Upon installing React package, I realized that it has small about of files.</p>
+
+      {/* <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div> */}
+      {/* <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p> */}
+    </>
+  )
+}
+
+export default App
