4f7333fd36b85e5496e87c752f69d10b5c425c13
F4UST-O3 <pantallanojojo1994@gmail.com>
2025-09-06 14:24:46 +0800

    Add control structures activity

---
+++ 11_javascript-refresher/06_control_structures.js
@@ -0,0 +1,31 @@
+//if else
+let grades = 75;
+if (grades >= 90) {
+    console.log(`'A'`);
+}
+else if (grades >= 80) {
+    console.log('B');
+}
+
+else if (grades >= 70) {
+    console.log('C');
+}
+
+else {
+    console.log('F');
+}
+
+
+// for loop
+for (let num = 1; num <= 5; num++) {
+    console.log(num);
+}
+
+
+// while loop
+let count = 0;
+while (count < 3) {
+    let greet = 'Hello!';
+    console.log(greet);
+    count++;
+}
