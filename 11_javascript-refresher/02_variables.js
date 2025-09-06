4a3bdf62a2793a6e5b424c43a5bbd24a02e3e5f0
F4UST-O3 <pantallanojojo1994@gmail.com>
2025-09-06 14:21:36 +0800

    Add variables activity

---
+++ 11_javascript-refresher/02_variables.js
@@ -0,0 +1,19 @@
+const name = "Jori";
+const age = 22;
+const isStudent = true;
+
+console.log(name, typeof name); // string
+console.log(age, typeof age);  // number/integer
+console.log(isStudent, typeof isStudent); // boolean
+
+//arithmetic operators
+let x = 7;
+let y = 3;
+console.log(x + y); // addition
+console.log(x - y); // subtraction  
+console.log(x * y); // multiplication
+console.log(x / y); // division
+
+//compare
+console.log("5" == 5);  //true, same value
+console.log("5" === 5); //false, different type
