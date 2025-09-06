c97f0f5f15823e27fae8995b273624e1208d06f6
F4UST-O3 <pantallanojojo1994@gmail.com>
2025-09-06 14:29:06 +0800

    Add let and const activity

---
+++ 11_javascript-refresher/10_let_const.js
@@ -0,0 +1,23 @@
+// let
+let name = "Jorilyn"; 
+let age = 21;
+console.log("Name:", name);
+console.log("Age:", age);
+
+name = "Jojo";
+age = 22;
+console.log("Updated Name:", name);
+console.log("Updated Age:", age);
+
+// const 
+const birthYear = 2003;
+console.log("Birth Year:", birthYear);
+
+// birthYear = 2004;
+// console.log("Birth Year:", birthYear); // error 
+
+// var
+var my_name = "Jori"; 
+var my_age = 22;
+my_age = 20;
+
