5550d18766f8a4eb2948d1a45708d9c57450c464
F4UST-O3 <pantallanojojo1994@gmail.com>
2025-09-06 14:31:40 +0800

    Add modules export activity

---
+++ 11_javascript-refresher/15_modules_export.js
@@ -0,0 +1,10 @@
+export default function greeting() {
+  return "Hello! This is my module.";
+}
+
+export const my_info = {  // ✅ changed to myInfo (camelCase)
+  name: "Jorilyn",
+  age: 21,
+  course: "BSIS"
+};
+
