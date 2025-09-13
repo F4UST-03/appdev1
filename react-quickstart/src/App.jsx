d789267d4ccb3322a4f8b4935adac6df35a61d91
F4UST-O3 <pantallanojojo1994@gmail.com>
2025-09-13 14:50:19 +0800

    Done displaying data

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -9,27 +9,33 @@ import './App.css'
 //   );
 // }
 
-function AboutPage() {
-  return (
-    <>
-      <h1>About</h1>
-      <p>Hello there.<br />How do you do?</p>
-    </>
-  );
-}
+// function AboutPage() {
+//   return (
+//     <>
+//       <h1>About</h1>
+//       <p>Hello there.<br />How do you do?</p>
+//     </>
+//   );
+// }
 
 
 function App() {
   // const [count, setCount] = useState(0)
-
+  const user = {
+    name: 'Jori',
+    age: 22,
+    avatarUrl: 'https://avatars.githubusercontent.com/u/184085946?v=4'
+  }
 
   return (
     <>
       {/* <h1>Welcome to My App!</h1>
       <MyButton /> */}
-      <AboutPage />
-      <img className="avatar" />
-
+      {/* <AboutPage /> */}
+      {/* <img className="avatar" /> */}
+      <h1>Hello {user.name}!</h1>
+      <p>Age: {user.age}</p>
+      <img className="avatar" src={user.avatarUrl}/>
     </>
   )
 }
