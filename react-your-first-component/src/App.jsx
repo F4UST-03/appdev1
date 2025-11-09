cc7f09563a10c8d48fd56bda754075d39dbe3857
F4UST-O3 <pantallanojojo1994@gmail.com>
2025-11-09 21:39:39 +0800

    Done with defining a component

---
+++ react-your-first-component/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <img
+      src="https://i.imgur.com/MK3eW3Am.jpg"
+      alt="Katherine Johnson"
+    />
+    </>
+  )
+}
+
+function Profile() { 
+  <div>
+    <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />
+  </div>
+ }
+
+export default App
