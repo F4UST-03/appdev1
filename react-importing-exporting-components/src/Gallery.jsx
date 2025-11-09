434b1472744ab200af327ac7a773024eb261003e
F4UST-O3 <pantallanojojo1994@gmail.com>
2025-11-09 22:07:07 +0800

    Done with exporting and importing multiple components from the same file

--- react-importing-exporting-components/src/Gallery.jsx
+++ react-importing-exporting-components/src/Gallery.jsx
@@ -1,4 +1,4 @@
-function Profile() {
+export function Profile() {
   return (
     <img
       src="https://i.imgur.com/QIrZWGIs.jpg"
@@ -17,3 +17,4 @@ export default function Gallery() {
     </section>
   );
 }
+
