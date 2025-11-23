41b05f1ff3a0e5b6d96a808dbd507062fab7b94a
F4UST-O3 <pantallanojojo1994@gmail.com>
2025-11-23 11:16:36 +0800

    Done with more fun with JavaScript objects and curly braces

--- react-javascript-in-jsx-with-curly-braces/src/App.jsx
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -10,24 +10,32 @@ function formatDate(date) {
   ).format(date);
 }
 
+const person = {
+  name: 'Gregorio Y. Zara',
+  theme: {
+    backgroundColor: 'black',
+    color: 'pink'
+  }
+};
+
 
 function TodoList() {
   return (
     <>
-      <h1>To Do List for {formatDate(today)}</h1>
-
-      <ul style={
-        {
-        backgroundColor: 'black',
-        color: 'pink'
-      }
-      }>
-        <li>Improve the videophone</li>
-        <li>Prepare aeronautics lectures</li>
-        <li>Work on the alcohol-fuelled engine</li>
-      </ul>
-    </>
-  
+      <div style={person.theme}>
+        <h1>{person.name}'s Todos for {formatDate(today)}</h1>
+        <img
+          className="avatar"
+          src="https://i.imgur.com/7vQD0fPs.jpg"
+          alt="Gregorio Y. Zara"
+        />
+        <ul>
+          <li>Improve the videophone</li>
+          <li>Prepare aeronautics lectures</li>
+          <li>Work on the alcohol-fuelled engine</li>
+        </ul>
+      </div>
+    </> 
   );
 }
 
